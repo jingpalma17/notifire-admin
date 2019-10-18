2371dd36ac1a8e1cba2831ef6f8b0157fb3148ce
Jingle Palma <jingpalma17@gmail.com>
2019-10-19 01:57:34 +0800

    Implement incident-report-feed component (Initial commit)

---
+++ src/app/incident-report-feed/incident-report-feed.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-incident-report-feed',
+  templateUrl: './incident-report-feed.component.html',
+  styleUrls: ['./incident-report-feed.component.scss']
+})
+export class IncidentReportFeedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
