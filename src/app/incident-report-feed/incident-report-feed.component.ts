a530353e4a28a5d9a778b80cdbf59cf433bb26cb
Jingle Palma <jingpalma17@gmail.com>
2019-10-19 03:43:20 +0800

    Display data from firebase

--- src/app/incident-report-feed/incident-report-feed.component.ts
+++ src/app/incident-report-feed/incident-report-feed.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FirebaseService } from '../services/firebase.service';
+
 @Component({
   selector: 'app-incident-report-feed',
   templateUrl: './incident-report-feed.component.html',
-  styleUrls: ['./incident-report-feed.component.scss']
+  styleUrls: ['./incident-report-feed.component.scss'],
 })
 export class IncidentReportFeedComponent implements OnInit {
+  feeds;
 
-  constructor() { }
+  constructor(private firebaseService: FirebaseService) {}
 
   ngOnInit() {
+    this.firebaseService.getIncidentReportFeed().subscribe(result => {
+      this.feeds = result;
+    });
   }
-
 }
