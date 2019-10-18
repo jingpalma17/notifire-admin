9d7afd15524352ff60609eba5d437142563e5646
Jingle Palma <jingpalma17@gmail.com>
2019-10-19 03:43:54 +0800

    Add route for incident report feed list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { IncidentReportFeedComponent } from './incident-report-feed/incident-report-feed.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: IncidentReportFeedComponent },
+  { path: 'incident-report-feed', component: IncidentReportFeedComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
