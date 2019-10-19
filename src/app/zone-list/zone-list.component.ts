11f8d9ae3ca1642f7f294e4c5796dbc0bfaab873
Jingle Palma <jingpalma17@gmail.com>
2019-10-19 18:07:51 +0800

    Implement zone-list component

---
+++ src/app/zone-list/zone-list.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material';
+
+import { FirebaseService } from '../services/firebase.service';
+import { ZoneInfoDialogComponent } from '../zone-info-dialog/zone-info-dialog.component';
+
+@Component({
+  selector: 'app-zone-list',
+  templateUrl: './zone-list.component.html',
+  styleUrls: ['./zone-list.component.scss'],
+})
+export class ZoneListComponent implements OnInit {
+  zones;
+
+  constructor(
+    private firebaseService: FirebaseService,
+    private dialog: MatDialog,
+  ) {}
+
+  ngOnInit() {
+    this.firebaseService.getZones().subscribe(result => {
+      this.zones = result;
+    });
+  }
+
+  addZone() {
+    this.dialog
+      .open(ZoneInfoDialogComponent, {
+        width: '400px',
+      })
+      .afterClosed()
+      .subscribe(result => {
+        if (result) {
+          // refresh
+        }
+      });
+  }
+
+  updateZone(zone) {
+    this.dialog
+      .open(ZoneInfoDialogComponent, {
+        width: '400px',
+        data: zone,
+      })
+      .afterClosed()
+      .subscribe(result => {
+        if (result) {
+          // refresh
+        }
+      });
+  }
+}
