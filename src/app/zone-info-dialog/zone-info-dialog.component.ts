57c4f7fa182300220e27173f2d6bddc0d66925b9
Jingle Palma <jingpalma17@gmail.com>
2019-10-19 18:08:48 +0800

    Implement adding and updating zone; Update zone fields

--- src/app/zone-info-dialog/zone-info-dialog.component.ts
+++ src/app/zone-info-dialog/zone-info-dialog.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { MAT_DIALOG_DATA, MatDialogRef, MatSnackBar } from '@angular/material';
+
+import { FirebaseService } from '../services/firebase.service';
 
 @Component({
   selector: 'app-zone-info-dialog',
@@ -8,13 +11,52 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class ZoneInfoDialogComponent implements OnInit {
   form: FormGroup;
+  zone;
+  title = 'Brand Info';
+  action = 'Apply';
 
-  constructor() {
+  constructor(
+    private dialogRef: MatDialogRef<ZoneInfoDialogComponent>,
+    private firebaseService: FirebaseService,
+    private snackbar: MatSnackBar,
+    @Inject(MAT_DIALOG_DATA) public data,
+  ) {
+    if (data) {
+      this.zone = data.payload.doc.data();
+      this.zone.id = data.payload.doc.id;
+    } else {
+      this.zone = {};
+      this.title = 'Add Brand';
+      this.action = 'Add';
+    }
     this.form = new FormGroup({
-      addressLine1: new FormControl(''),
-      addressLine2: new FormControl(''),
+      code: new FormControl(this.zone.code || ''),
+      name: new FormControl(this.zone.name || ''),
+      description: new FormControl(this.zone.description || ''),
     });
   }
 
   ngOnInit() {}
+
+  save(zone) {
+    if (this.zone.id) {
+      this.update({ ...zone, id: this.zone.id });
+    } else {
+      this.create(zone);
+    }
+  }
+
+  create(zone) {
+    this.firebaseService.addZone(zone).then(result => {
+      this.dialogRef.close();
+      this.snackbar.open('Zone added', 'Ok', { duration: 3000 });
+    });
+  }
+
+  update(zone) {
+    this.firebaseService.updateZone(zone).then(result => {
+      this.dialogRef.close(true);
+      this.snackbar.open('Zone info updated', 'Ok', { duration: 3000 });
+    });
+  }
 }
