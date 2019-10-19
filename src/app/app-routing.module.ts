d0d7467aaf6f66c1f9168c2e2946500e1b0bd97c
Jingle Palma <jingpalma17@gmail.com>
2019-10-19 20:16:46 +0800

    Add route for zone list page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { IncidentReportFeedComponent } from './incident-report-feed/incident-report-feed.component';
 import { IncidentReportsComponent } from './incident-reports/incident-reports.component';
+import { ZoneListComponent } from './zone-list/zone-list.component';
 
 const routes: Routes = [
   { path: '', component: IncidentReportFeedComponent },
   { path: 'incident-report-feed', component: IncidentReportFeedComponent },
   { path: 'incident-reports', component: IncidentReportsComponent },
+  { path: 'zones', component: ZoneListComponent },
 ];
 
 @NgModule({
